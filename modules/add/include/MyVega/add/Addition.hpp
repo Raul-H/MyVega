3b44c73819ecce5c421d54352d9d9218d9ee8632
Raul-H <raulhabibullin@yandex.ru>
2024-09-27 09:44:16 +0300

    feat(::): doc

--- modules/add/include/MyVega/add/Addition.hpp
+++ modules/add/include/MyVega/add/Addition.hpp
@@ -2,9 +2,20 @@
 
 namespace arith {
 
+/**
+ * @brief Class for adding two numbers
+ * 
+ */
 class Addition
 {
 public:
+/**
+ * @brief Calculate the sum of two numbers
+ * 
+ * @param a first term
+ * @param b second term
+ * @return double 
+ */
     double compute( double a, double b );
 };
 
--- modules/multiply/include/MyVega/multiply/Multiplication.hpp
+++ modules/multiply/include/MyVega/multiply/Multiplication.hpp
@@ -2,9 +2,20 @@
 
 namespace arith {
 
+/**
+ * @brief Class for calculating the product of two numbers
+ * 
+ */
 class Multiplication
 {
 public:
+/**
+ * @brief Calculate the product of two numbers
+ * 
+ * @param a first multiplier
+ * @param b second multiplier
+ * @return double 
+ */
     double compute( double a, double b );
 };
 
